e886ab8599eaf894b58279444c2825d2de4e0f56
Jaf-Seven <mohamedjaafar2297@gmail.com>
2025-10-21 20:41:13 +0530

    ci_cd_added

--- src/mlProject/pipeline/stage_02_data_validation.py
+++ src/mlProject/pipeline/stage_02_data_validation.py
@@ -18,7 +18,7 @@ class DataValidationTrainingPipeline:
 
 if __name__ == '__main__':
     try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        logger.info(f">>>>>> stage {STAGE_NAME} started again <<<<<<")
         obj = DataValidationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
